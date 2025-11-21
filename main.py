bf2a4667db6c421b6ba7f719b23840efb95b6a02
Donderbuis <nico.maritz05@gmail.com>
2025-11-21 10:19:06 +1000

    Added word count functionality, also learn how to inform lsp of expected data types for function parameters and returns.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # Python executable main file
 
-def get_book_text(path_to_file):
+def get_book_text(path_to_file: str) -> str:
 
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
+def book_word_count(text_in_file: str) -> int:
+    return len(text_in_file.split(" "))
+
 def main():
 
     path_to_book = "./books/frankenstein.txt"
+    book_text = get_book_text(path_to_book)
+    word_count = book_word_count(book_text)
 
     print(get_book_text(path_to_book))
+    print(f"Word count: {word_count}")
 
 main()
 
