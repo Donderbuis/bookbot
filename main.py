fdf82c1adee9542f5299fd1e0c48977c07477b36
Donderbuis <nico.maritz05@gmail.com>
2025-11-21 13:11:41 +1000

    Added sorted letter occurence that list them in order of most used and also removes non alphabet letters. Made an algorithm to print the information neatly to the terminal.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python executable main file
 
-from stats import book_word_count, book_word_occurences
+from stats import book_word_count, book_char_occurences, sort_char_occurences
 
 def get_book_text(path_to_file: str) -> str:
 
@@ -10,13 +10,20 @@ def get_book_text(path_to_file: str) -> str:
 
 def main():
 
-    path_to_book = "./books/frankenstein.txt"
+    path_to_book = "books/frankenstein.txt"
     book_text = get_book_text(path_to_book)
     word_count = book_word_count(book_text)
-    char_occurences: dict[str, int] = book_word_occurences(book_text)
+    char_occurences: dict[str, int] = book_char_occurences(book_text)
+    sorted_occurences: list[dict[str, str | int]] = sort_char_occurences(char_occurences)
 
-    print(char_occurences)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_occurences:
+        print(f"{char_dict["char"]}: {char_dict["num"]}")
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@
 def book_word_count(book_text: str) -> int:
     return len(book_text.split())
 
-def book_word_occurences(book_text: str) -> dict[str, int]:
+def book_char_occurences(book_text: str) -> dict[str, int]:
     charset: set[str] = set()
     chardict: dict[str, int] = {}
 
@@ -16,4 +16,20 @@ def book_word_occurences(book_text: str) -> dict[str, int]:
             chardict[char] = 1
 
     return chardict
+
+def sort_on(chars: dict[str, str | int]):
+    return chars["num"]
+
+def sort_char_occurences(char_occurences: dict[str, int]) -> list[dict[str, str | int]]:
+    ordered_list: list[dict[str, str | int]] = []
+
+    for char in char_occurences:
+        if not char.isalpha():
+            continue
+        refactor_dict: dict[str, str | int] = {"char": char, "num": 0}
+        refactor_dict["num"] = char_occurences[char]
+        ordered_list.append(refactor_dict)
+
+    ordered_list.sort(reverse=True, key=sort_on)
+    return ordered_list
 
